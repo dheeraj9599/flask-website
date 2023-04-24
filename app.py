3983973cc1299d008659375270a498b2dfdfd9c0
dheeraj9599 <92773853+dheeraj9599@users.noreply.github.com>
2023-04-24 17:17:15 +0000

    Nothing

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 # Flask is the class
 from flask import Flask, render_template, jsonify, request
 from db import Load_Jobs_From_DB, Load_Job_From_DB
-app = Flask(__name__) # __name__ shows how a particular app is invocked
+
+app = Flask(__name__)  # __name__ shows how a particular app is invocked
 
 #path after domain name ex- jovian.com/profile , domain - jovian.com/ and url after it use in route
+
 
 @app.route("/")
 def hello_world():
@@ -11,27 +13,29 @@ def hello_world():
   JOBS = Load_Jobs_From_DB()
   return { "hello":"bye"}
 
+
 # to convert DB rows as dict use this v imp
 @app.route("/api")
 def list_jobs():
   JOBS = Load_Jobs_From_DB()
   return jsonify(JOBS)
 
+
 # Dynamic route
 @app.route("/job/<id>")
 def list_specific_job(id):
   JOB = Load_Job_From_DB(id)
-  return render_template('jobpage.html', job=JOB)
+  return { "hello":"bye"
+    }
 
 
-@app.route("/job/<id>/apply", methods = ['post'])
+@app.route("/job/<id>/apply", methods=['post'])
 def apply_for_job(id):
   # data = request.args # only get data from url
-  data = request.form #get data from post method submission
+  data = request.form  #get data from post method submission
   new_data = jsonify(data)
 
-  
-  return render_template('application_submitted.html', application = new_data)
+  return render_template('application_submitted.html', application=new_data)
 
 
 if __name__ == '__main__':
